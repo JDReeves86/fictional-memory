4b51ad90abf2f4d1c9d756d86c01268fce68fb46
JDReeves86 <Reeves.Jacobd@gmail.com>
2022-09-27 15:30:42 -0700

    adds DEL request for tags by id

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -127,6 +127,7 @@ router.delete('/:id', async (req, res) => {
     res.status(200).json({message: "No product to delete!"});
     return;
   }
+  res.status(200).json(removedProd)
   } catch(err) {res.status(200).json(err)}
 });
 
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -44,8 +44,21 @@ router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try{
+    const removedTag = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!removedTag) {
+      res.status(200).json({message: "No tags by that ID found!"});
+      return;
+    }
+    res.status(200).json(removedTag)
+  }
+  catch(err) {res.status(500).json(err)}
 });
 
 module.exports = router;
