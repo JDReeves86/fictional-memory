860260367676a9be3c411935283a10049f0abb7d
JDReeves86 <Reeves.Jacobd@gmail.com>
2022-09-27 15:39:34 -0700

    adds PUT request for tags

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -47,8 +47,26 @@ router.post('/', async (req, res) => {
   catch(err) {res.status(500).json(err)}
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const changedTag = await Tag.update({
+      tag_name: req.body.tagName
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!changedTag) {
+      res.status(200).json({message: "No matching tags found!"});
+      return;
+    }
+    res.status(200).json(changedTag)
+  } 
+  catch(err) {res.status(500).json(err)}
+  
+
 });
 
 router.delete('/:id', async (req, res) => {
