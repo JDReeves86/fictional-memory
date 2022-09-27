499ab6a23a5c6179ef74e3c7999c8679ac6c319c
JDReeves86 <Reeves.Jacobd@gmail.com>
2022-09-27 15:34:00 -0700

    adding POST request for tags

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -36,8 +36,15 @@ router.get('/:id', async (req, res) => {
   catch(err) {res.status(500).json(err)}
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const newTag = await Tag.create({
+      tag_name: req.body.tagName
+    })
+    res.status(200).json(newTag)
+  } 
+  catch(err) {res.status(500).json(err)}
 });
 
 router.put('/:id', (req, res) => {
